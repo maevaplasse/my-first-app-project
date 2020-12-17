063e7b27055d66707df856da0540b56df1a0e0ee
maevaplasse <74125126+maevaplasse@users.noreply.github.com>
2020-12-17 15:53:23 +1100

    Move existing JS from codesandbox to GitHub

---
+++ script.js
@@ -0,0 +1,83 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+  return `${day} ${hours}:${minutes}`;
+}
+// current full date and time informations
+function search(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city");
+  let cityInput = document.querySelector("#city-input");
+  cityElement.innerHTML = cityInput.value;
+  searchCity(cityInput.value);
+}
+// selected city input searching form
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+// conversion from celsius to fahrenheit
+let dateElement = document.querySelector("h1");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+// calling new date
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+// calling new city input
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+// calling fahrenheit conversion
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+// calling celsius conversion
+function searchCity(city) {
+  let apiKey = "80ff9ab07927e65d043ab3591c9e3c20";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+searchCity("Santa Barbara");
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let city = response.data.name;
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = city;
+  let h3 = document.querySelector("#temperature");
+  h3.innerHTML = temperature;
+}
+function retrievePosition(position) {
+  let apiKey = "80ff9ab07927e65d043ab3591c9e3c20";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showTemperature);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(retrievePosition);
+}
+let currentLocationButton = document.querySelector("#geolocation");
+currentLocationButton.addEventListener("click", getCurrentLocation);
